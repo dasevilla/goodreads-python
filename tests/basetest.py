a5852e37e52555b8c5d32e394a4469b76315fcf9
Devin Sevilla <dasevilla@gmail.com>
2011-07-14 10:23:24 -0700

    Whitespace fix

--- tests/basetest.py
+++ tests/basetest.py
@@ -2,6 +2,7 @@ import unittest
 
 from goodreads import GoodReadsClient
 
+
 class BaseTestCase(unittest.TestCase):
 
     GOODREADS_API_KEY = "GOODREADS_API_KEY"
@@ -10,7 +11,7 @@ class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.assertIsNotNone(self.GOODREADS_API_KEY)
         self.assertIsNotNone(self.GOODREADS_API_SECRET)
-        
+
         self.client = GoodReadsClient(
             self.GOODREADS_API_KEY,
             self.GOODREADS_API_SECRET)
